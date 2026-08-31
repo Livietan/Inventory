19637d04baf69aaee91e0c8143b871e15fcda940
akmalrasyid53@gmail.com <akmalrasyid53@gmail.com>
2026-08-31 18:23:45 +0700

    update

---
+++ database/system.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+connect = sqlite3.connect("data/data.db")
+
+cursor = connect.cursor()
+
+cursor.execute("""
+CREATE TABLE user (
+ID AUTO INCREMENT PRIMARY KEY INT,
+username VARCHAR(64),
+password VARCHAR(32)
+);
+""")
+
+connect.commit()
+connect.close()

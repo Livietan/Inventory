04b015bf51ccd4fa8177e05b1baf1763e2c14c46
akmalrasyid53@gmail.com <akmalrasyid53@gmail.com>
2026-09-01 07:47:50 +0700

    update

--- javascript/access.js
+++ javascript/access.js
@@ -4,6 +4,21 @@ const registerSubmit = document.getElementById("register");
 export const username = document.getElementById("username");
 export const password = document.getElementById("password");
 
+var adminContent = document.getElementById("admin-content");
+var managerContent = document.getElementById("manager-content");
+var containerAdmin = document.getElementById("container-admin");
+var containerManager = document.getElementById("container-manager");
+
+adminContent.addEventListener("click", () => {
+  containerAdmin.style.display = "none";
+  containerManager.style.display = "flex";
+});
+
+managerContent.addEventListener("click", () => {
+  containerAdmin.style.display = "flex";
+  containerManager.style.display = "none";
+});
+
 registerSubmit.addEventListener("click", () => {
-  register()
+  register();
 });

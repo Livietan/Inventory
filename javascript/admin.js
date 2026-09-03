fdb1f8b7898d72ccbd35d4fc5d099d7668e5d786
akmalrasyid53@gmail.com <akmalrasyid53@gmail.com>
2026-09-03 21:15:21 +0700

    a

--- javascript/admin.js
+++ javascript/admin.js
@@ -2,9 +2,9 @@ var firstMenu = document.getElementById("first-menu");
 var secondaryMenu = document.getElementById("secondary-menu");
 
 firstMenu.addEventListener("click", () => {
-    window.location.href = "manager-dashboard.html"
+    window.location.href = "admin-dashboard.html"
 })
 
 secondaryMenu.addEventListener("click", () => {
-    window.location.href = "manager-admin.html"
+    window.location.href = "admin-package.html"
 })

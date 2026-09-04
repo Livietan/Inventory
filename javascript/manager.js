76a04f186167dbf9d112606e14c16bb300ccdaf9
akmalrasyid53@gmail.com <akmalrasyid53@gmail.com>
2026-09-04 09:00:31 +0700

    update

--- javascript/manager.js
+++ javascript/manager.js
@@ -2,8 +2,18 @@ var firstMenu = document.getElementById("first-menu");
 var secondaryMenu = document.getElementById("secondary-menu");
 var logoutMenu = document.getElementById("logout-menu");
 var userTag = document.getElementById("userTag");
+var blockPopup = document.getElementById("block-popup");
+var closeMenu = document.getElementById("close");
+var user = sessionStorage.getItem("user");
+var role = sessionStorage.getItem("role");
 
-userTag.textContent = sessionStorage.getItem("user");
+userTag.textContent = user;
+
+if (role == "manager") {
+  blockPopup.style.display = "none";
+} else {
+  blockPopup.style.display = "flex";
+}
 
 firstMenu.addEventListener("click", () => {
   window.location.href = "manager-dashboard.html";
@@ -13,6 +23,10 @@ secondaryMenu.addEventListener("click", () => {
   window.location.href = "manager-admin.html";
 });
 logoutMenu.addEventListener("click", () => {
-  sessionStorage.removeItem("user");
+  sessionStorage.clear();
+  window.location.href = "access.html";
+});
+closeMenu.addEventListener("click", () => {
+  sessionStorage.clear();
   window.location.href = "access.html";
 });

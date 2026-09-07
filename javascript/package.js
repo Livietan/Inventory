363d7fd070e2b896a71aaf3f0c79c3172dec0561
akmalrasyid53@gmail.com <akmalrasyid53@gmail.com>
2026-09-07 21:03:21 +0700

    update

--- javascript/package.js
+++ javascript/package.js
@@ -1,10 +1,53 @@
 var dashboard = document.getElementById("dashboard");
+var package = document.getElementById("package");
+var setting = document.getElementById("setting");
 var logout = document.getElementById("log-out");
+var profileSetting = document.getElementById("profile-setting");
+var informationDashboard = document.getElementById("information-dashboard");
+var informationPackage = document.getElementById("information-package");
+var informationSetting = document.getElementById("information-setting");
+var informationLogout = document.getElementById("information-logout");
 
 dashboard.addEventListener("click", () => {
   window.location.href = "dashboard.html";
 });
+dashboard.addEventListener("mouseenter", () => {
+  var x = dashboard.getBoundingClientRect();
+  informationDashboard.style.display = "block";
+  informationDashboard.style.top = `${x.top + 10}px`;
+  informationDashboard.style.left = `${x.left + 60}px`;
+});
+dashboard.addEventListener("mouseleave", () => {
+  informationDashboard.style.display = "none";
+});
+package.addEventListener("mouseenter", () => {
+  var x = package.getBoundingClientRect();
+  informationPackage.style.display = "block";
+  informationPackage.style.top = `${x.top + 10}px`;
+  informationPackage.style.left = `${x.left + 60}px`;
+});
+package.addEventListener("mouseleave", () => {
+  informationPackage.style.display = "none";
+});
+setting.addEventListener("mouseenter", () => {
+  var x = setting.getBoundingClientRect();
+  informationSetting.style.display = "block";
+  informationSetting.style.top = `${x.top + 10}px`;
+  informationSetting.style.left = `${x.left + 60}px`;
+});
+setting.addEventListener("mouseleave", () => {
+  informationSetting.style.display = "none";
+});
 logout.addEventListener("click", () => {
   sessionStorage.clear();
   window.location.href = "access.html";
+});
+logout.addEventListener("mouseenter", () => {
+  var x = logout.getBoundingClientRect();
+  informationLogout.style.display = "block";
+  informationLogout.style.top = `${x.top + 10}px`;
+  informationLogout.style.left = `${x.left + 60}px`;
+});
+logout.addEventListener("mouseleave", () => {
+  informationLogout.style.display = "none";
 });

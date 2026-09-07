195162d70d16925809f0ee419f458e10aa2ce13c
akmalrasyid53@gmail.com <akmalrasyid53@gmail.com>
2026-09-07 20:31:10 +0700

    update

--- javascript/dashboard.js
+++ javascript/dashboard.js
@@ -1,9 +1,15 @@
+var dashboard = document.getElementById("dashboard");
 var package = document.getElementById("package");
-var tagTime = document.getElementById("datetime");
+var setting = document.getElementById("setting");
 var logout = document.getElementById("log-out");
+var tagTime = document.getElementById("datetime");
 var userTag = document.getElementById("userTag");
 var ellipsis = document.getElementById("ellipsis");
 var profileSetting = document.getElementById("profile-setting");
+var informationDashboard = document.getElementById("information-dashboard");
+var informationPackage = document.getElementById("information-package");
+var informationSetting = document.getElementById("information-setting");
+var informationLogout = document.getElementById("information-logout");
 
 var datetime = new Date();
 var day = datetime.getDay();
@@ -22,12 +28,49 @@ var dayList = [
 
 tagTime.textContent = `${dayList[day]} ${date}/${mont}/${year}`;
 userTag.textContent = `${sessionStorage.getItem("firstName")} ${sessionStorage.getItem("lastName")}`;
+
+dashboard.addEventListener("mouseenter", () => {
+  var x = dashboard.getBoundingClientRect();
+  informationDashboard.style.display = "block";
+  informationDashboard.style.top = `${x.top + 10}px`;
+  informationDashboard.style.left = `${x.left + 60}px`;
+});
+dashboard.addEventListener("mouseleave", () => {
+  informationDashboard.style.display = "none";
+});
 package.addEventListener("click", () => {
   window.location.href = "package.html";
+});
+package.addEventListener("mouseenter", () => {
+  var x = package.getBoundingClientRect();
+  informationPackage.style.display = "block";
+  informationPackage.style.top = `${x.top + 10}px`;
+  informationPackage.style.left = `${x.left + 60}px`;
+});
+package.addEventListener("mouseleave", () => {
+  informationPackage.style.display = "none";
+});
+setting.addEventListener("mouseenter", () => {
+  var x = setting.getBoundingClientRect();
+  informationSetting.style.display = "block";
+  informationSetting.style.top = `${x.top + 10}px`;
+  informationSetting.style.left = `${x.left + 60}px`;
+});
+setting.addEventListener("mouseleave", () => {
+  informationSetting.style.display = "none";
 });
 logout.addEventListener("click", () => {
   sessionStorage.clear();
   window.location.href = "access.html";
+});
+logout.addEventListener("mouseenter", () => {
+  var x = logout.getBoundingClientRect();
+  informationLogout.style.display = "block";
+  informationLogout.style.top = `${x.top + 10}px`;
+  informationLogout.style.left = `${x.left + 60}px`;
+});
+logout.addEventListener("mouseleave", () => {
+  informationLogout.style.display = "none";
 });
 ellipsis.addEventListener("click", () => {
   if (profileSetting.style.display == "none") {
